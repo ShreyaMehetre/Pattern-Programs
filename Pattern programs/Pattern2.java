49a38e77a8cc5e18a0d2bb73ca7cf5465fa45e1f
shreyaMehetre <shreyamehetre17@gmail.com>
2024-08-12 18:05:29 +0530

    pattern2 program

---
+++ Pattern programs/Pattern2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class pattern2{
+	public static void main(String[]agrs){
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int i=0;i<num;i++){
+			for(int j=0;j<=num-i;j++){
+				System.out.print("* "); 
+	}
+	System.out.println();
+	
+}}}
