0abcf6a24443382c0a597ff9522170adbfbacd99
shreyamehetre2000 <shreyamehetre21@gmail.com>
2024-08-12 15:35:09 +0530

    program1 added

---
+++ Pattern programs/Pattern1.java
@@ -0,0 +1,11 @@
+java.util.Scanner;
+public class pattern1{
+	public Static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int i=0;i<num;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("* ");
+	}System.out.println();
+}
+}}
